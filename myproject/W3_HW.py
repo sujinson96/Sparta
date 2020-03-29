38d1f2e9a32fbd6c78697b1d558fbcb98ebf66f1
sujinson96 <61937552+sujinson96@users.noreply.github.com>
2020-03-29 20:26:50 +0900

    W3_HW_수정

--- myproject/W3_HW.py
+++ myproject/W3_HW.py
@@ -8,11 +8,13 @@ data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20200
 soup = BeautifulSoup(data.text, 'html.parser')
 
 chart = soup.select('div#body-content > div.newest-list > div.music-list-wrap > table.list-wrap > tbody > tr.list')
-ranking = 0
+# ranking = 0
 for songs in chart:
-    ranking += 1
+    # ranking += 1
     title = songs.select_one('td.info > a.title.ellipsis')
+    ranking = songs.select_one('td.number')
+    ranking2 = ranking.text
     artists = songs.select_one('td.info> a.artist.ellipsis')
     if title is not None:
-        print(ranking, "/", title.text.lstrip(), "/", artists.text)
+        print(ranking2.split('\n')[0], "/", title.text.lstrip(), "/", artists.text)
 
